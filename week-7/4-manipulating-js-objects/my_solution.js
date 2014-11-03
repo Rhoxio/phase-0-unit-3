ee01d7625eb73ea94258997997e07fd3a688b8d8
Kevin <azorius07@yahoo.com>
2014-11-02 21:05:30 -0800

    week 7 push

--- week-7/4-manipulating-js-objects/my_solution.js
+++ week-7/4-manipulating-js-objects/my_solution.js
@@ -1,6 +1,6 @@
  // U3.W7: Manipulating JavaScript Objects
 
-// I worked on this challenge: [by myself, with: ]
+// I worked on this challenge: with Brittney Hodson
 
 // There is a section below where you will write your code.
 
@@ -15,6 +15,51 @@ var terah = {
 }
 // __________________________________________
 // Write your code below.
+var adam = {
+  name: "Adam",
+  spouse: terah,
+}
+
+
+terah.spouse = adam;
+terah.weight = 125
+delete terah.eyeColor
+
+terah.children = {
+  carson: {
+    name: "Carson"},
+  carter: {
+    name: "Carter"},
+  colton: {
+    name: "Colton"}
+
+}
+
+adam.children = terah.children
+
+
+
+
+
+
+
+
+// var adam = {
+//   name: "Adam",
+//   spouse: terah
+
+// // }
+
+// // terah.spouse = adam;
+// // terah.weight = 125;
+// // delete terah.eyeColor
+// // terah.children = {
+
+// // }
+
+
+
+
 
 
 
@@ -25,12 +70,10 @@ var terah = {
 
 // __________________________________________
 // Reflection: Use the reflection guidelines
-//
-//
-//
-//
-//
-//
+// For the most part, this was a fairly straight-forward. The
+// only thing Birttany and I really snagged on for any amount of time 
+// was how to get an object inside of an object. It was easier than we 
+// thought, to be honest. You literally just slip the damn thing in there. 
 
 
 // __________________________________________
@@ -58,7 +101,7 @@ assert(
 
 assert(
   terah.spouse === adam,
-  "terah should have a spouse property with the value of the variable adam.",
+  "terah should have a spouse property with the value of the object adam.",
   "3. "
 )
 
@@ -69,45 +112,63 @@ assert(
 )
 
 assert(
-  terah.eyeColor === undefined,
-  "The terah eyeColor property should be removed.",
+  terah.eyeColor === undefined || null,
+  "The terah eyeColor property should be deleted.",
   "5. "
 )
 
 assert(
   terah.spouse.spouse === terah,
-  "The terah spouse property's value spouse property should be terah.",
+  "Terahs spouses spouse property should refer back to the terah.",
   "6. "
 )
 
 assert(
   (terah.children instanceof Object),
-  "The value of the terah children property should be an Object.",
+  "The value of the terah children property should be defined as an Object.",
   "7. "
 )
 
 assert(
-  terah.children.carson.name === "Carson",
-  "The terah children property should have a carson property with its own property name with a value of 'Carson'.",
+  (terah.children.carson instanceof Object),
+  "carson should be defined as an object and assigned as a child of Terah",
   "8. "
-)
+  )
 
 assert(
-  terah.children.carter.name === "Carter",
-  "The terah children property should have a carter property with its own property name with a value of 'Carter'.",
+  terah.children.carson.name === "Carson",
+  "Terah's children should include an object called carson which has a name property equal to 'Carson'.",
   "9. "
 )
 
 assert(
-  terah.children.colton.name === "Colton",
-  "The terah children property should have a colton property with its own property name with a value of 'Colton'.",
+  (terah.children.carter instanceof Object),
+  "carter should be defined as an object and assigned as a child of Terah",
   "10. "
+  )
+
+assert(
+  terah.children.carter.name === "Carter",
+  "Terah's children should include an object called carter which has a name property equal to 'Carter'.",
+  "11. "
+)
+
+assert(
+  (terah.children.colton instanceof Object),
+  "carter should be defined as an object and assigned as a child of Terah",
+  "12. "
+  )
+
+assert(
+  terah.children.colton.name === "Colton",
+  "Terah's children should include an object called colton which has a name property equal to 'Colton'.",
+  "13. "
 )
 
 assert(
   adam.children === terah.children,
-  "The value of the adam children property should be the value of the terah children property",
-  "11. "
+  "The value of the adam children property should be equal to the value of the terah children property",
+  "14. "
 )
 
 console.log("\nHere is your final terah object:")
